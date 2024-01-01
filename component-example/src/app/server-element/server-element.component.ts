0dea65e13cf66f33fb5be99f73d02d142bf4fd28
Leenez <jani_leinonen@hotmail.com>
2024-01-01 22:00:22 +0200

    component-example-hooks

--- component-example/src/app/server-element/server-element.component.ts
+++ component-example/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ContentChild, DoCheck, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +6,54 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrl: './server-element.component.css',
   encapsulation: ViewEncapsulation.Emulated // None, Native
 })
-export class ServerElementComponent implements OnInit{
+export class ServerElementComponent implements 
+  OnInit, 
+  OnChanges, 
+  DoCheck, 
+  AfterContentChecked, 
+  AfterViewChecked, 
+  AfterViewInit,
+  AfterContentInit,
+  OnDestroy {
   @Input('srvElement') element: {type: string, name: string, content: string}
+  @Input() name: string;
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef; 
 
-  constructor() {}
+  constructor() {
+    console.log("constructor called")
+  }
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngAfterViewChecked(): void {
+    
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("ngonchanges called")
+    console.log(changes)
+  }
 
   ngOnInit() {
+    console.log("ngoninit called")
+    console.log(this.header.nativeElement.textContent)
+  }
+
+  ngDoCheck() {
+    console.log("ngdocheck called")
+  }
+
+  ngAfterContentInit() {
+    console.log("ngaftercontentinit called")
+    console.log(this.header.nativeElement.textContent)
+  }
+
+  ngAfterContentChecked(): void {
+    
+  }
+
+  ngAfterViewInit(): void {
     
   }
 
