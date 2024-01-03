eca5470d08de117883eccb48d80adb2d37de1704
Leenez <jani_leinonen@hotmail.com>
2024-01-03 19:31:08 +0200

    ex4

---
+++ ex4/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'ex4';
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  onIntervalFired(firedNumber: number) {
+    console.log(firedNumber)
+    if(firedNumber % 2 === 0) { this.evenNumbers.push(firedNumber) }
+    else {this.oddNumbers.push(firedNumber)}
+  }
+}
