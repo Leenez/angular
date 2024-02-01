d79680a685538ae5f0faa25b034ca20f8c8c260e
unknown <jani_leinonen@hotmail.com>
2024-02-01 09:49:51 +0200

    reactive-form

--- forms-ex/src/app/app.component.ts
+++ forms-ex/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,8 +7,12 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  onSubmit(form: NgForm) {
-    console.log(form)
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  selectedSubscription = 'Advanced'
+  @ViewChild('signUpForm') sgnForm:NgForm
+
+  onSubmit() {
+    console.log(this.sgnForm.value)
   }
 
 }
