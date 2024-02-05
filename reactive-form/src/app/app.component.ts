355b02d68f5edf34564cb4e1919eace45e80d914
unknown <jani_leinonen@hotmail.com>
2024-02-05 14:32:19 +0200

    reactive form error

--- reactive-form/src/app/app.component.ts
+++ reactive-form/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
+  signUpForm: FormGroup
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.signUpForm = new FormGroup({
+      'userData':new FormGroup ({
+        'username':new FormControl(null, Validators.required),
+        'email':new FormControl(null, [Validators.required, Validators.email])
+      }),
+      'gender':new FormControl('male'),
+      'hobbies':new FormArray([])
+    })
+  }
+
+  onSubmit() {
+    console.log(this.signUpForm)
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signUpForm.get('hobbies')).push(control);
+  }
+
 }
